9791ff3638962fa8cd7200fd3386dc07d1d1f532
Matthias Bolte <matthias@tinkerforge.com>
2019-05-20 19:26:53 +0200

    shell: Escape positional argument to avoid option parsing

--- software/examples/shell/example-numeric-value.sh
+++ software/examples/shell/example-numeric-value.sh
@@ -6,4 +6,4 @@ uid=XYZ # Change XYZ to the UID of your Segment Display 4x7 Bricklet 2.0
 tinkerforge call segment-display-4x7-v2-bricklet $uid set-brightness 7 # Set to full brightness
 
 # Show "- 42" on the Display
-tinkerforge call segment-display-4x7-v2-bricklet $uid set-numeric-value -2,-1,4,2
+tinkerforge call segment-display-4x7-v2-bricklet $uid set-numeric-value -- -- -2,-1,4,2
