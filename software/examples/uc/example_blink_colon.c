7c569b1c5c1fb1219e6ce4d08b585e3d23c9833c
Erik Fleckstein <erik@tinkerforge.com>
2020-08-25 15:54:47 +0200

    uc: Fix whitespace

--- software/examples/uc/example_blink_colon.c
+++ software/examples/uc/example_blink_colon.c
@@ -1,14 +1,9 @@
-
-
 #include "bindings/hal_common.h"
 #include "bindings/bricklet_segment_display_4x7_v2.h"
 
 #define UID "XYZ" // Change XYZ to the UID of your Segment Display 4x7 Bricklet 2.0
 
 void check(int rc, const char* msg);
-
-
-
 
 TF_SegmentDisplay4x7V2 sd;
 
