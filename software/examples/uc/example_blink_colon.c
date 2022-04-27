5d165cf21549d7ce25b73353eccfddfdd63e32e2
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 15:38:53 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_blink_colon.c
+++ software/examples/uc/example_blink_colon.c
@@ -1,23 +1,19 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_segment_display_4x7_v2.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_segment_display_4x7_v2.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Segment Display 4x7 Bricklet 2.0
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 static TF_SegmentDisplay4x7V2 sd;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_segment_display_4x7_v2_create(&sd, UID, hal), "create device object");
+	check(tf_segment_display_4x7_v2_create(&sd, NULL, hal), "create device object");
 
 	check(tf_segment_display_4x7_v2_set_brightness(&sd,
 	                                               7), "call set_brightness"); // Set to full brightness
